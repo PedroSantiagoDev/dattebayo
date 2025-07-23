2f18165b64aee5a9a4ad0b854c72dbbce15a44f5
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-07-22 21:58:43 -0300

    minha primeira rota

---
+++ src/main/java/com/jphp/CadastroDeNinjas/Controller/NinjaController.java
@@ -0,0 +1,14 @@
+package com.jphp.CadastroDeNinjas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+    @GetMapping("/")
+    public String index() {
+        return "Boas vindas";
+    }
+}
