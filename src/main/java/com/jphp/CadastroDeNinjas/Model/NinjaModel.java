de4d64d04151b4c97defe65bcb461aff0e9043a4
Joao Pedro Santiago Lima <JT0094360@MATEUS.DC>
2025-07-24 09:15:53 -0300

    criando a entity tb_cadastro a parti do ninja model

---
+++ src/main/java/com/jphp/CadastroDeNinjas/Model/NinjaModel.java
@@ -0,0 +1,24 @@
+package com.jphp.CadastroDeNinjas.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String nome;
+    public String email;
+    public int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
