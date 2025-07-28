07213171335d38dc843aca45ebbd68ced5462b2c
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-07-28 16:14:10 -0300

    fix: coloca a columna email como UNIQUE

--- src/main/java/com/jphp/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/jphp/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -17,8 +17,11 @@ public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String dificuldade;
+
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 }
--- src/main/java/com/jphp/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/jphp/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -15,9 +15,14 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
+
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Foreing Key
     private MissoesModel missoes;
